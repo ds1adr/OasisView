62e713211d6c655a7bdcedb278101f8385a36f0f
Wontai Ki <ds1adr@gmail.com>
2026-04-04 23:59:02 -0700

    First Commit

---
+++ build-OASISView-Desktop-Debug/CMakeFiles/OASISView.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OASISView_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/OASISView_autogen.dir/ParseCache.txt"
+  "OASISView_autogen"
+  "CMakeFiles/OASISView.dir/MainWindow.cpp.o"
+  "CMakeFiles/OASISView.dir/MainWindow.cpp.o.d"
+  "CMakeFiles/OASISView.dir/OASISView_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/OASISView.dir/OASISView_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/OASISView.dir/main.cpp.o"
+  "CMakeFiles/OASISView.dir/main.cpp.o.d"
+  "OASISView"
+  "OASISView.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OASISView.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-OASISView-Desktop-Debug/CMakeFiles/OASISView_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OASISView_autogen"
+  "OASISView_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/OASISView_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

0705b402bada8b4c23b74cafe2c04b1de7fb5510
Wontai Ki <ds1adr@gmail.com>
2026-06-02 00:03:37 -0700

    updating ILT dialog

--- OASISView/ILTDialog.cpp
+++ OASISView/ILTDialog.cpp
@@ -12,10 +12,36 @@ ILTDialog::ILTDialog(QWidget *parent)
     mExposureLayout->addWidget(mHLine, 4, 0, 1, 2);
 
     QIntValidator *intValidator = new QIntValidator();
+
+    mFlipGridLabel = new QLabel();
+    mFlipGridLabel->setText("Flip Grid (nm):");
+
+    mFlipGrid = new QLineEdit();
+    mFlipGrid->setValidator(intValidator);
+
+    mExposureLayout->addWidget(mFlipGridLabel, 5, 0);
+    mExposureLayout->addWidget(mFlipGrid, 5, 1);
+
+    mMaxCountLabel = new QLabel();
+    mMaxCountLabel->setText("Max ILT Count:");
+
+    mMaxCount = new QLineEdit();
+    mMaxCount->setValidator(intValidator);
+
+    mExposureLayout->addWidget(mMaxCountLabel, 6, 0);
+    mExposureLayout->addWidget(mMaxCount, 6,1);
+
+    delete intValidator;
 }
 
 ILTDialog::~ILTDialog() {
+    delete mHLine;
 
+    delete mFlipGridLabel;
+    delete mFlipGrid;
+
+    delete mMaxCountLabel;
+    delete mMaxCount;
 }
 
 void ILTDialog::runButtonClicked(bool) {

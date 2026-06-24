19d76394d419eb186a57857bc9f3c013efab9e72
Wontai Ki <ds1adr@gmail.com>
2026-06-24 00:35:47 -0700

    MaskDataViewDialog

--- OASISView/MaskDataViewDialog.cpp
+++ OASISView/MaskDataViewDialog.cpp
@@ -1,5 +1,7 @@
 #include "MaskDataViewDialog.h"
 
+#include <QPainter>
+
 MaskDataViewDialog::MaskDataViewDialog(QWidget *parent)
     : QDialog{parent}
 {
@@ -17,11 +19,25 @@ void MaskDataViewDialog::paintEvent(QPaintEvent *event) {
 
     double ratioX = rect.width() / (config.Nx * config.dx);
     double ratioY = rect.height() / (config.Ny * config.dy);
-    double ratio = std::min(ratioX, ratioY);
+    int ratio = (int)std::min(ratioX, ratioY);
+
+    QPainter painter;
+    painter.begin(this);
+
+    painter.setBrush(QBrush(Qt::blue, Qt::Dense1Pattern));
 
     for (int y = 0; y < config.Ny; y++) {
         for (int x = 0; x < config.Nx; x++) {
+            double value = mask[y * config.Nx + x];
+            if (value == 0) continue;
 
+            int px = x * ratio;
+            int py = y * ratio;
+
+            QRect rect = QRect(px, py, ratio, ratio);
+            painter.drawRect(rect);
         }
     }
+
+    painter.end();
 }

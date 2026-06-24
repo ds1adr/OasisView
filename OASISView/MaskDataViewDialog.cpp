e3d4c9d3a9bf1bbdc9290437af12fbdfb9e3e704
Wontai Ki <ds1adr@gmail.com>
2026-06-23 23:12:34 -0700

    making mask view for ILT

--- OASISView/MaskDataViewDialog.cpp
+++ OASISView/MaskDataViewDialog.cpp
@@ -7,5 +7,21 @@ MaskDataViewDialog::MaskDataViewDialog(QWidget *parent)
 }
 
 void MaskDataViewDialog::updateMask(SimulationConfig& config, std::vector<double>& mask) {
+    this->config = config;
+    this->mask = mask;
+    update();
+}
 
+void MaskDataViewDialog::paintEvent(QPaintEvent *event) {
+    QRect rect = this->geometry();
+
+    double ratioX = rect.width() / (config.Nx * config.dx);
+    double ratioY = rect.height() / (config.Ny * config.dy);
+    double ratio = std::min(ratioX, ratioY);
+
+    for (int y = 0; y < config.Ny; y++) {
+        for (int x = 0; x < config.Nx; x++) {
+
+        }
+    }
 }
--- OASISView/MaskDataViewDialog.h
+++ OASISView/MaskDataViewDialog.h
@@ -8,10 +8,16 @@
 class MaskDataViewDialog : public QDialog
 {
     Q_OBJECT
+
+private:
+    SimulationConfig config;
+    std::vector<double> mask;
 public:
     explicit MaskDataViewDialog(QWidget *parent = nullptr);
 
     void updateMask(SimulationConfig& config, std::vector<double>& mask);
+
+    void paintEvent(QPaintEvent *event) override;
 
 signals:
 
